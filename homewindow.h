9d0a403328ea202913790634c150b96913008656
Bakht17 <61829647+Bakht17@users.noreply.github.com>
2024-10-10 07:32:28 +0300

    Add files via upload

---
+++ homewindow.h
@@ -0,0 +1,34 @@
+#ifndef HOMEWINDOW_H
+#define HOMEWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include "loginwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class HomeWindow;
+}
+QT_END_NAMESPACE
+
+class HomeWindow : public QMainWindow {
+    Q_OBJECT
+
+   public:
+    HomeWindow(QWidget *parent = nullptr);
+    ~HomeWindow();
+
+   private slots:
+    void on_registerButton_clicked();
+    void on_loginButton_clicked();
+
+   private:
+    Ui::HomeWindow *ui;
+    QSqlDatabase db;
+    LoginWindow *loginWindow = nullptr;
+
+   public:
+    bool connectToDatabase();
+};
+
+#endif  // HOMEWINDOW_H
